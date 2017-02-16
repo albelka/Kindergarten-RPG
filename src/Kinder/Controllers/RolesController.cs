353e4df92024b36aec49e71bdc7166c94f95fd91
Anne Belka and Bryant Wang <anne+bryant@>
2017-02-16 09:57:20 -0800

    Add ability to edit and delete Roles.

--- src/Kinder/Controllers/RolesController.cs
+++ src/Kinder/Controllers/RolesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
 using Kinder.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -37,13 +38,48 @@ namespace Kinder.Controllers
         {
             try
             {
-                var roleName = HttpContext.Request.Form["RoleName"];
+                string roleName = HttpContext.Request.Form["RoleName"];
                 _db.Roles.Add(new IdentityRole()
                 {
-                    Name = roleName
+                    Name = roleName,
+                    NormalizedName = roleName.ToUpper()
                 });
                 _db.SaveChanges();
                 ViewBag.ResultMessage = "Role created successfully!";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Delete(string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            _db.Roles.Remove(thisRole);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(string id)
+        {
+            var thisRole = _db.Roles.Where(r => r.Id == id).FirstOrDefault();
+            return View(thisRole);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(IdentityRole role)
+        {
+            try
+            {
+                var thisRole = _db.Roles.Where(r => r.Id == role.Id).FirstOrDefault();
+                thisRole.Name = role.Name;
+                thisRole.NormalizedName = role.Name.ToUpper();
+                _db.Entry(thisRole).State = EntityState.Modified;
+                _db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
             catch
